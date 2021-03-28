8e22fe341a1541da605ad6fce10e599542cac5b3
Yarik-eest <ya_rick@ukr.net>
2021-03-28 12:35:34 +0300

    all complete

--- server/server.js
+++ server/server.js
@@ -3,12 +3,38 @@ import express from 'express'
 import path from 'path'
 import cors from 'cors'
 import bodyParser from 'body-parser'
+import axios from 'axios'
 
 import cookieParser from 'cookie-parser'
 import Html from '../client/html'
 
 const port = process.env.PORT || 3000
 const server = express()
+const { readFile, writeFile, unlink } = require('fs').promises
+
+const setHeaders = (req, res, next) => {
+  res.set('x-skillcrucial-user', 'dda3ec26-90b3-4233-87cb-6a23f0e1faab')
+  res.set('Access-Control-Expose-Headers', 'X-SKILLCRUCIAL-USER')
+  return next()
+}
+
+const saveFile = async (users) => {
+  await writeFile(`${__dirname}/test.json`, JSON.stringify(users), { encoding: 'utf8' })
+}
+
+const deleteFile = () => {
+  unlink(`${__dirname}/test.json`)
+}
+
+const fileRead = async () => {
+  return readFile(`${__dirname}/test.json`, { encoding: 'utf8' })
+    .then((data) => JSON.parse(data))
+    .catch(async () => {
+      const { data: users } = await axios('https://jsonplaceholder.typicode.com/users')
+      await saveFile(users)
+      return users
+    })
+}
 
 server.use(cors())
 
@@ -17,6 +43,47 @@ server.use(bodyParser.urlencoded({ limit: '50mb', extended: true, parameterLimit
 server.use(bodyParser.json({ limit: '50mb', extended: true }))
 
 server.use(cookieParser())
+server.use(setHeaders)
+
+server.get('/api/v1/users/', async (req, res) => {
+  const users = await fileRead()
+  res.json(users)
+})
+
+server.post('/api/v1/users/', async (req, res) => {
+  const users = await fileRead()
+  const newUserData = req.body
+  newUserData.id = users.length + 1
+  const newUsersArray = [...users, newUserData]
+  saveFile(newUsersArray)
+  res.json({ status: 'success', id: newUserData.id })
+})
+
+server.patch('/api/v1/users/:userId', async (req, res) => {
+  const users = await fileRead()
+  const userData = req.body
+  const { userId } = req.params
+  const newUsersArray = users.map((item) => {
+    return item.id === +userId ? Object.assign(item, userData) : item
+  })
+  saveFile(newUsersArray)
+  res.json({ status: 'success', id: userId })
+})
+
+server.delete('/api/v1/users/:userId', async (req, res) => {
+  const users = await fileRead()
+  const { userId } = req.params
+  const newUsersArray = users.filter((item) => {
+    return item.id !== +userId
+  })
+  saveFile(newUsersArray)
+  res.json({ status: 'success', id: userId })
+})
+
+server.delete('/api/v1/users/', (req, res) => {
+  deleteFile()
+  res.json('file deleted')
+})
 
 server.use('/api/', (req, res) => {
   res.status(404)
